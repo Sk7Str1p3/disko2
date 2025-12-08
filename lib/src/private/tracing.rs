39ad3af649b471b72a761ec52bb3500b92b73915
Sk7Str1p3 <finenkofedor@gmail.com>
2025-12-08 19:29:54 +0000

    feat(lib::tracing): custom log formatter

--- lib/src/private/tracing.rs
+++ lib/src/private/tracing.rs
@@ -48,10 +48,158 @@ mod time {
     }
 }
 
+mod format {
+    //! ## Format
+    //!
+    //! Format final log message
+
+    use std::fmt::Result;
+
+    use console::{
+        Term,
+        measure_text_width,
+        strip_ansi_codes
+    };
+    use owo_colors::OwoColorize as _;
+    use tracing::{
+        Event,
+        Level,
+        Subscriber
+    };
+    use tracing_subscriber::fmt::format::Writer;
+    use tracing_subscriber::fmt::time::FormatTime;
+    use tracing_subscriber::fmt::{
+        FmtContext,
+        FormatEvent,
+        FormatFields,
+        FormattedFields
+    };
+    use tracing_subscriber::registry::LookupSpan;
+
+    pub(super) struct TracingFormatter;
+
+    impl<S, F> FormatEvent<S, F> for TracingFormatter
+    where
+        S: Subscriber + for<'a> LookupSpan<'a>,
+        F: for<'a> FormatFields<'a> + 'static
+    {
+        fn format_event(
+            &self,
+            ctx: &FmtContext<'_, S, F>,
+            mut wr: Writer<'_>,
+            event: &Event<'_>
+        ) -> Result {
+            let meta = event.metadata();
+
+            let left = {
+                let mut buf = String::new();
+                let mut wr = Writer::new(&mut buf);
+
+                // Time
+                super::time::TimeFormatter.format_time(&mut wr)?;
+
+                // Log level
+                let level = match *meta.level() {
+                    Level::TRACE => "TRACE".purple().to_string(),
+                    Level::DEBUG => "DEBUG".blue().to_string(),
+                    Level::INFO => " INFO".green().to_string(),
+                    Level::WARN => " WARN".yellow().bold().to_string(),
+                    Level::ERROR => "ERROR".red().bold().to_string()
+                };
+                write!(wr, " {} ", level.dimmed())?;
+
+                // Message
+                ctx.format_fields(wr.by_ref(), event)?;
+
+                buf
+            };
+
+            let right = {
+                let mut buf = String::new();
+                let mut wr = Writer::new(&mut buf);
+
+                // Target
+                write!(wr, "{}", meta.target().purple().dimmed())?;
+
+                // Spans and their extensions
+                if let Some(scope) = ctx.event_scope() {
+                    write!(wr, "{}", "(".purple().dimmed())?;
+                    let mut spans = Vec::new();
+                    for span in scope.from_root() {
+                        let mut span_info = String::new();
+                        span_info.push_str(&span.metadata().name().dimmed().to_string());
+
+                        if let Some(fields) = span.extensions().get::<FormattedFields<F>>()
+                            && !fields.is_empty()
+                        {
+                            let fields = {
+                                let mut f = Vec::new();
+                                let pairs = fields.fields.split(' ').collect::<Vec<_>>();
+                                for pair in pairs {
+                                    let pair = strip_ansi_codes(pair);
+                                    let (key, value) = pair.split_once('=').unwrap();
+                                    let key = key.cyan();
+                                    let value = value.cyan();
+                                    let value = value.bold();
+
+                                    f.push(format!("{key}: {value}").dimmed().to_string())
+                                }
+                                f
+                            };
+                            span_info.push_str(&format!(
+                                "{}{}{}",
+                                "(".dimmed(),
+                                fields.join(&", ".dimmed().to_string()),
+                                ")".dimmed()
+                            ));
+                        }
+                        spans.push(span_info);
+                    }
+                    write!(
+                        wr,
+                        "{}{}",
+                        spans.join(&", ".dimmed().to_string()),
+                        ")".purple().dimmed()
+                    )?;
+                };
+
+                // Location in source code
+                write!(
+                    wr,
+                    " {} ",
+                    format!(
+                        "{}:{}",
+                        meta.file().unwrap_or("<unknown>.rs").blue(),
+                        meta.line()
+                            .map(|l| l.to_string())
+                            .unwrap_or("??".into())
+                            .blue()
+                    )
+                    .dimmed()
+                    .underline()
+                )?;
+
+                buf
+            };
+
+            let width = measure_text_width(&left) + measure_text_width(&right);
+            let term_width = Term::stdout().size().1 as usize;
+            let spaces = if width < term_width {
+                term_width - width
+            } else {
+                1
+            };
+            writeln!(wr, "{left}{}{right}", " ".repeat(spaces))?;
+
+            Ok(())
+        }
+    }
+}
+
 /// Install trace dispatcher
 pub fn install() -> Result<()> {
     let subscriber = tracing_subscriber::fmt()
-        .with_timer(time::TimeFormatter)
+        .event_format(format::TracingFormatter)
         .finish();
 
     tracing::subscriber::set_global_default(subscriber).context("Failed to install log")
